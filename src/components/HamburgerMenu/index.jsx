4591f52fed707db9a98f8c0677a2c95c2a557ddf
ElpixJunior <nasurov.eldar@mail.ru>
2019-12-10 02:05:35 +0300

    Adding hamburger menu

--- src/components/HamburgerMenu/index.jsx
+++ src/components/HamburgerMenu/index.jsx
@@ -8,7 +8,7 @@ class Example extends React.Component {
 }
 
   render () {
-    var styles = {
+    const styles = {
         bmBurgerButton: {
           position: 'fixed',
           width: '25px',
@@ -23,18 +23,21 @@ class Example extends React.Component {
           background: '#a90000'
         },
         bmCrossButton: {
-          height: '24px',
-          width: '24px'
+          height: '30px',
+          width: '30px'
         },
         bmCross: {
-          background: '#bdc3c7'
+          background: '#fff',
+          height: '20px',
+          width: '4px'
         },
         bmMenuWrap: {
           position: 'fixed',
-          height: '100%'
+          height: '100%',
+          top: 0,
         },
         bmMenu: {
-          background: '#373a47',
+          background: 'linear-gradient( 237.26deg, rgba(87, 29, 163, 0.8) 0%, rgba(65, 29, 213, 0.82) 100% )',
           padding: '2.5em 1.5em 0',
           fontSize: '1.15em'
         },
@@ -43,10 +46,15 @@ class Example extends React.Component {
         },
         bmItemList: {
           color: '#b8b7ad',
-          padding: '0.8em'
+          padding: '0.8em',
+          display: 'flex',
+          flexDirection: 'column',
         },
         bmItem: {
-          display: 'inline-block'
+          display: 'inline-block',
+          marginBottom: '20px',
+          fontWeight: 700,
+          color: '#fff',
         },
         bmOverlay: {
           background: 'rgba(0, 0, 0, 0.3)'
@@ -55,10 +63,11 @@ class Example extends React.Component {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
         <Menu styles={styles} width="100%" right>
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+              <a href="/decions">Решения</a>
+              <a href="/support">Поддержка</a>
+              <a href="/partners">Партнеры</a>
+              <a href="/auth">Личный кабинет</a>
+              <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
         </Menu>
     );
   }
