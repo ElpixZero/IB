c2f0f99fe42ef7c55a4ba09c4898371faf698e31
ElpixJunior <nasurov.eldar@mail.ru>
2019-12-10 01:44:32 +0300

    Refactored

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import HamburgerMenu from '../HamburgerMenu';
 import Logo from '../../img/logo.png';
 import './index.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-      <header className="header"> 
-        <div className="leftHeader">
-          <img src={Logo} className="logo" alt="Logo" />
-          <a>IB</a>
-        </div>
-        <div className="centerHeader">
-          <a>Решения</a>
-          <a>Поддержка</a>
-          <a>Партнеры</a>
-        </div>
-        <div className="rightHeader">
-          <a href="/auth">Личный кабинет</a>
-          <div className="hamburgerMenu">
+    <header className="header">
+      <div className="leftHeader">
+        <img src={Logo} className="logo" alt="Logo" />
+        <a href="#">IB</a>
+      </div>
+      <div className="centerHeader">
+        <a href="/decions">Решения</a>
+        <a href="/support">Поддержка</a>
+        <a href="/partners">Партнеры</a>
+      </div>
+      <div className="rightHeader">
+        <Link to="/auth">Личный кабинет</Link>
+        <div className="hamburgerMenu">
           <HamburgerMenu />
-          </div>
         </div>
-      </header>
+      </div>
+    </header>
   );
 }
 
